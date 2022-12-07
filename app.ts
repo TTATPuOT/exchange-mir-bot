4d5fd00de3152460060e8c40a5fe71af3f4a384e
AntohaOutside <neverov12@gmail.com>
2022-12-08 02:21:21 +0600

    Init

---
+++ app.ts
@@ -0,0 +1,14 @@
+import * as http from 'http'
+
+const host = 'localhost'
+const port = 8000
+
+const requestListener = (req: any, res: any) => {
+	res.writeHead(200)
+	res.end('Hello, world!')
+}
+
+const server = http.createServer(requestListener)
+server.listen(port, host, () => {
+	console.log(`Server is running on http://${host}:${port}`)
+})
